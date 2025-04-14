0c53c9c66dfca8e3cdb8202e1bc12a1134cc1a21
Konrad <konradrajman@gmail.com>
2025-04-15 01:08:12 +0200

    Basic Cybersecurity Journal

---
+++ DLPFinalProjectIdea/Models/JournalEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace DLPFinalProjectIdea.Models
+{
+    public class JournalEntry
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+
+}
