4ccfa407edd1ea8b1aa085ba64020416316bb724
PolaQ9 <konradrajman@gmail.com>
2025-05-07 02:06:46 +0100

    Added another quiz. Fixed some visual bugs on the congratulation/better luck next time. Added images and navigation button to the congratualtion/better luck next time at the end of the quiz

--- DLPFinalProjectIdea/Pages/QuizCardBase.cs
+++ DLPFinalProjectIdea/Pages/QuizCardBase.cs
@@ -30,7 +30,7 @@ namespace DLPFinalProjectIdea.Pages
 
         private void LoadQuestions()
         {
-            
+
 
             if (QuizId == "cyber-awareness")
             {
@@ -146,12 +146,114 @@ namespace DLPFinalProjectIdea.Pages
 
                 var q2 = new Question
                 {
+                    QuestionTitle = "What is a malware?",
+                    Options = new List<string> { "A software that helps your computer", "Anti-Virus", "Harmful software designed to damage or steal data", "Hardware component in the computer" },
+                    Answer = "Harmful software designed to damage or steal data"
+                };
+
+                var q3 = new Question
+                {
                     QuestionTitle = "Which of these is a type of malware?",
                     Options = new List<string> { "SQL Injection", "Phishing", "Ransomware", "Firewall" },
                     Answer = "Ransomware"
                 };
 
-                Questions.AddRange(new List<Question> { q1, q2 });
+                var q4 = new Question
+                {
+                    QuestionTitle = "What does a virus do to a computer?",
+                    Options = new List<string> { "Spreads and damages files or system", "Makes it run faster", "Protects it from hackers", "Allows the computer to access the internet" },
+                    Answer = "Spreads and damages files or system"
+                };
+
+                var q5 = new Question
+                {
+                    QuestionTitle = "What does Ransomware do to the computer?",
+                    Options = new List<string> { "Helps to update windows", "Program that locks your files and waits until you pay money", "Makes the internet faster", "Fake emails" },
+                    Answer = "Program that locks your files and waits until you pay money"
+                };
+
+                var q6 = new Question
+                {
+                    QuestionTitle = "What is spyware?",
+                    Options = new List<string> { "Software that fixes your computer", "Video call software", "App that blocks pop-ups", "Software that secretly collets your data" },
+                    Answer = "Software that secretly collets your data"
+                };
+
+                var q7 = new Question
+                {
+                    QuestionTitle = "What is DDoS?",
+                    Options = new List<string> { "System update", "Method to store data", "An attack that overwhelms the servers, making them have really bad performance", "A way to connect to the cloud" },
+                    Answer = "An attack that overwhelms the servers, making them have really bad performance"
+                };
+
+                var q8 = new Question
+                {
+                    QuestionTitle = "Which of these is not a malware type",
+                    Options = new List<string> { "Virus", "Firewall", "Worm", "Ransomware" },
+                    Answer = "Firewall"
+                };
+
+                Questions.AddRange(new List<Question> { q1, q2, q3, q4, q5, q6, q7, q8 });
+            }
+            else if (QuizId == "internetsafety")
+            {
+                var q1 = new Question
+                {
+                    QuestionTitle = "Why is it important to check for https and the secure lock on a website?",
+                    Options = new List<string> { "It means website has passwords", "Means the site will be fast", "It means that the site is secure and data is encrypted", "The site will load faster" },
+                    Answer = "It means that the site is secure and data is encrypted"
+                };
+
+                var q2 = new Question
+                {
+                    QuestionTitle = "What should you do before clicking on a link in an email or message",
+                    Options = new List<string> { "Click and check what it is", "Send it to your coworker", "Check who sent it and if it looks suspicious", "Save it on the computer" },
+                    Answer = "Check who sent it and if it looks suspicious"
+                };
+
+                var q3 = new Question
+                {
+                    QuestionTitle = "What is the best way to keep your online accounts secure",
+                    Options = new List<string> { "Use strong, unique passwords and enable 2FA", "Use your birthday as a password", "Use the same password everywhere", "Share your password with your friends" },
+                    Answer = "Use strong, unique passwords and enable 2FA"
+                };
+
+                var q4 = new Question
+                {
+                    QuestionTitle = "What should you avoid when using public Wi-Fi?",
+                    Options = new List<string> { "Browsing through Social Media", "Checking the Weather", "Watch TikTok", "Logging into your bank or work accounts" },
+                    Answer = "Logging into your bank or work accounts"
+                };
+
+                var q5 = new Question
+                {
+                    QuestionTitle = "What if you receive unexpected email asking for personal info, what should you do?",
+                    Options = new List<string> { "Delete it or Report it as phishing", "Click on the links provided in the email", "Post it on social media", "Tell your coworker to click on the link, to check it" },
+                    Answer = "Delete it or Report it as phishing"
+                };
+
+                var q6 = new Question
+                {
+                    QuestionTitle = "Why you should regularly update your apps and software?",
+                    Options = new List<string> { "To make them look nicer", "To fix bugs and patch security issues", "To add more functions", "To tell your friends you have newer version" },
+                    Answer = "To fix bugs and patch security issues"
+                };
+
+                var q7 = new Question
+                {
+                    QuestionTitle = "Which of these is a safe online behavior?",
+                    Options = new List<string> { "Sharing your passwords with coworkers", "Downloading apps from unknown websites", "Logging out of accounts when finished", "Posting your account details on social media" },
+                    Answer = "Logging out of accounts when finished"
+                };
+
+                var q8 = new Question
+                {
+                    QuestionTitle = "What information should you not share on public websites?",
+                    Options = new List<string> { "Your favourite color", "Your full name & address", "Your opinion about movies", "What food you like" },
+                    Answer = "Your full name & address"
+                };
+
+                Questions.AddRange(new List<Question> { q1, q2, q3, q4, q5, q6, q7, q8 });
             }
         }
     }
